6bbbcfe344dc0ea3709800293f5f34f4a3ca58de
hyewon Ahn <neonoclock213@naver.com>
2025-11-16 16:56:28 +0900

    Title: 비교 연산, Time: 69ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251116/비교 연산/comparison-operator.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(' ');
+
+let A = Number(input[0]);
+let B = Number(input[1]);
+
+console.log(A>=B ? 1 : 0);
+console.log(A>B ? 1 : 0);
+console.log(A<=B ? 1 : 0);
+console.log(A<B ? 1 : 0);
+console.log(A===B ? 1 : 0);
+console.log(A!==B ? 1 : 0);
