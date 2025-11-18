589f5432803803d99604af7d9b5f789465e13c53
hyewon Ahn <neonoclock213@naver.com>
2025-11-18 14:29:28 +0900

    Title: 중앙값 구하기, Time: 67ms, Memory: 10MB, Status: Passed - Codetree

--- 251118/중앙값 구하기/find-the-median.js
+++ 251118/중앙값 구하기/find-the-median.js
@@ -1,5 +1,9 @@
-const fs = require("fs");
-let [a, b, c] = fs.readFileSync(0).toString().trim().split(" ").map(Number);
+const fs = require("fs")
+let input = fs.readFileSync(0).toString().trim().split(" ")
+
+let a = Number(input[0])
+let b = Number(input[1])
+let c = Number(input[2])
 
 if ((a > b && a < c) || (a > c && a < b)) {
     console.log(a);      // a가 중간
