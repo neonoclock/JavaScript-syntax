4fdb19b5ac8cc34678d28e6cfd2255297d959e64
hyewon Ahn <neonoclock213@naver.com>
2025-11-19 17:38:11 +0900

    Title: 제곱하여 출력하기, Time: 68ms, Memory: 10MB, Status: Passed - Codetree

--- 251119/제곱하여 출력하기/print-square-of-elements.js
+++ 251119/제곱하여 출력하기/print-square-of-elements.js
@@ -4,6 +4,8 @@ let input = fs.readFileSync(0).toString().trim().split("\n")
 let N = Number(input[0])
 let arr = input[1].split(" ").map(Number)
 
+let result = ""
+
 for (let i=0; i<N; i++) {
     result += (arr[i] * arr[i] + " ")
 }
