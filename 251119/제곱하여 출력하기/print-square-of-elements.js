15e2ae9337dae94b0b76ac2254e403063cfd1036
hyewon Ahn <neonoclock213@naver.com>
2025-11-19 17:37:13 +0900

    Title: 제곱하여 출력하기, Time: 65ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 251119/제곱하여 출력하기/print-square-of-elements.js
@@ -0,0 +1,11 @@
+const fs = require("fs")
+let input = fs.readFileSync(0).toString().trim().split("\n")
+
+let N = Number(input[0])
+let arr = input[1].split(" ").map(Number)
+
+for (let i=0; i<N; i++) {
+    result += (arr[i] * arr[i] + " ")
+}
+
+console.log(result.trim());
