eeeff26cd15e8c631fa99b89ec1fcb934564c400
Hyewon Ahn <neonoclock213@naver.com>
2026-06-02 15:42:01 +0900

    [Passed] 숫자의 증감 (51ms, 16MB)

---
+++ 260602/숫자의 증감/increasing-and-decreasing-numbers.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+C, N = input().split()
+N = int(N)
+
+if C == 'A':
+    i = 1
+    while i <= N:
+        print(i, end=' ')
+        i += 1
+
+elif C == 'D':
+    i = N
+    while i >= 1:
+        print(i, end=' ')
+        i -= 1
