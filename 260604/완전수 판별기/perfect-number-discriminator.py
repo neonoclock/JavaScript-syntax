b7b371e14d349a22a349f4b938d1c5af9948ad7f
Hyewon Ahn <neonoclock213@naver.com>
2026-06-04 13:58:44 +0900

    [Passed] 완전수 판별기 (51ms, 16MB)

---
+++ 260604/완전수 판별기/perfect-number-discriminator.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+sum_val = 0
+
+for i in range(1, N):
+    if N % i == 0:
+        sum_val += i
+
+if sum_val == N:
+    print("P")
+else:
+    print("N")
