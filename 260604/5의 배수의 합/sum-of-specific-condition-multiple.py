afd9a500e100dc63e500be92553450acb9e49011
Hyewon Ahn <neonoclock213@naver.com>
2026-06-04 13:38:58 +0900

    [Passed] 5의 배수의 합 (47ms, 16MB)

---
+++ 260604/5의 배수의 합/sum-of-specific-condition-multiple.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+A, B = map(int, input().split())
+
+sum_val = 0
+
+if A>B:
+    for i in range(B, A+1):
+        if i%5==0:
+            sum_val += i
+    print(sum_val)
+else:
+    for i in range(A, B+1):
+        if i%5==0:
+            sum_val += i
+    print(sum_val)
