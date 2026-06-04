03ac2b59c7ad45e17b92400cdfc59ccb087b9ec8
Hyewon Ahn <neonoclock213@naver.com>
2026-06-04 15:04:04 +0900

    [Passed] 조건에 부합하는 수 (49ms, 16MB)

---
+++ 260604/조건에 부합하는 수/number-that-meets-the-condition.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+A = int(input())
+
+for i in range(1, A+1):
+    if i%2==0 and i%4!=0:
+        continue
+    if (i//8)%2==0:
+        continue
+    if (i%7)<4:
+        continue
+    print(i, end=' ')
