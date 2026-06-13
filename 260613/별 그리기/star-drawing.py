0255c32f769ac26afcc3ec48a7fe8b02f108e943
Hyewon Ahn <neonoclock213@naver.com>
2026-06-13 17:14:00 +0900

    [Passed] 별 그리기 (52ms, 16MB)

---
+++ 260613/별 그리기/star-drawing.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+for i in range(1, N+1):
+    print(" " * (N - i) + "*" * (2 * i - 1))
+
+for i in range(1, N):
+    print(" "*i + "*" * (2 * (N - i) - 1))
