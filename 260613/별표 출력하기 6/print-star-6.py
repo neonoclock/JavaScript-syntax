0acfec07f93b10a2c4b1c1873d09c08168e128d8
Hyewon Ahn <neonoclock213@naver.com>
2026-06-13 16:45:51 +0900

    [Passed] 별표 출력하기 6 (51ms, 16MB)

---
+++ 260613/별표 출력하기 6/print-star-6.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+for i in range(N):
+    print("  " * i + "* " * ((2 * N - 1) - 2 * i))
+
+for i in range(N - 2, -1, -1):
+    print("  " * i + "* " * ((2 * N - 1) - 2 * i))
